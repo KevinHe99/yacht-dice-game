e2aa23e1b9b575b610b37c844d86e4050763f381
Kevin He <kevin66he@hotmail.com>
2025-05-17 15:56:10 -0400

    Generate Dices onto browser

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-
-
+import {useState} from "react";
+import {nanoid} from "nanoid";
 
 export default function App() {
+
+  const [diceData, setDiceData] = useState(() => generateDice())
+
+  function generateDice() {
+    return new Array(5)
+      .fill(0)
+      .map(() => (
+        {
+          value: Math.ceil(Math.random() * 6),
+          id: nanoid()
+        }
+      ))
+  }
+
+  const diceObjs = diceData.map(item => (
+    <button key={item.id}>{item.value}</button>
+  ))
+
+
   return (
-    <h1>Hello World</h1>
-  )
+    <>
+      <h1>Yacht Dice Game</h1>
+      <div>
+        {diceObjs}
+      </div>
+      <button>Roll</button>
+    </>
+  );
 }
