5f892178e2a434cd89fadfcd59c004d3396b05a6
Kevin He <kevin66he@hotmail.com>
2025-05-17 16:37:51 -0400

    Added Ability to hold dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
-import {useState} from "react";
-import {nanoid} from "nanoid";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import Die from "./component/Die.jsx";
 
 export default function App() {
-
-  const [diceData, setDiceData] = useState(() => generateDice())
+  const [diceData, setDiceData] = useState(() => generateDice());
 
   function generateDice() {
-    return new Array(5)
-      .fill(0)
-      .map(() => (
-        {
-          value: Math.ceil(Math.random() * 6),
-          isHeld: false,
-          id: nanoid()
-        }
-      ))
+    return new Array(5).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
   }
 
-  const diceObjs = diceData.map(item => (
-    <Die key={item.id} value={item.value} held={item.isHeld}/>
-  ))
+  function rollDice() {
+    setDiceData((prevState) =>
+      prevState.map((item) =>
+        item.isHeld ? item : { ...item, value: Math.ceil(Math.random() * 6) },
+      ),
+    );
+  }
 
+  function holdDie(id) {
+    setDiceData((prevState) =>
+      prevState.map((item) =>
+        item.id === id ? { ...item, isHeld: !item.isHeld } : item,
+      ),
+    );
+  }
+
+  const diceObjs = diceData.map((item) => (
+    <Die
+      key={item.id}
+      value={item.value}
+      held={item.isHeld}
+      handleOnClick={() => holdDie(item.id)}
+    />
+  ));
 
   return (
     <>
       <h1>Yacht Dice Game</h1>
-      <div>
-        {diceObjs}
-      </div>
-      <button>Roll</button>
+      <div>{diceObjs}</div>
+      <button onClick={() => rollDice()}>Roll</button>
     </>
   );
 }
--- src/component/Die.jsx
+++ src/component/Die.jsx
@@ -1,17 +1,17 @@
-
-
-export default function Die({value, held}) {
+export default function Die({ value, held, handleOnClick }) {
   const style = {
-    backgroundColor : held ? "#5BC3EB" : "#EDE6E3",
-    padding: '12px 20px',
-    border: '2px solid #36382E',
-    borderRadius: '8px',
-    cursor: 'pointer',
-    fontSize: '16px',
-    margin: '4px',
-  }
+    backgroundColor: held ? "#5BC3EB" : "#EDE6E3",
+    padding: "12px 20px",
+    border: "2px solid #36382E",
+    borderRadius: "8px",
+    cursor: "pointer",
+    fontSize: "16px",
+    margin: "4px",
+  };
 
   return (
-    <button style={style}>{value}</button>
-  )
+    <button style={style} onClick={handleOnClick}>
+      {value}
+    </button>
+  );
 }
